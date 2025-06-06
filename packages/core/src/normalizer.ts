86360efdcd5d8521d7acd03e204abcafe2a64fea
Matej Bransky <matej.bransky@ataccama.com>
2025-06-06 15:31:44 +0200

    create normalize fn

---
+++ packages/core/src/normalizer.ts
@@ -0,0 +1,39 @@
+import type { DereferencedPaths } from './dereference';
+import type { Rule } from './rules';
+import { traverse } from './traverse';
+import type {
+	LinkedJSONSchema,
+	NormalizedJSONSchema,
+} from './types/JSONSchema';
+
+// FIXME: Update type once you know what `options` are needed
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Options = any;
+
+type NormalizeArgs = {
+	rootSchema: LinkedJSONSchema;
+	dereferencedPaths: DereferencedPaths;
+	fileName: string;
+	rules: Map<string, Rule>;
+	options: Options;
+};
+
+/**
+ * Normalizes a JSON Schema (draft 7) by applying a series of transformation rules.
+ */
+export function normalize({
+	rootSchema,
+	dereferencedPaths,
+	fileName,
+	rules,
+	options,
+}: NormalizeArgs): NormalizedJSONSchema {
+	rules.forEach((rule) =>
+		traverse({
+			schema: rootSchema,
+			callback: (schema, key) =>
+				rule({ schema, fileName, options, key, dereferencedPaths }),
+		})
+	);
+	return rootSchema as NormalizedJSONSchema;
+}
